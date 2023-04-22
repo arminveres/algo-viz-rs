e1413eb91f40a2692fda4563c6ce1c5a6fbd3463
Armin Veres <armin.veres@hotmail.com>
2023-04-22 16:59:06 +0200

    feat: bubbe sort with visualization

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,20 @@ use rand::{self, Rng};
 // TODO: (aver) implement dynamic screen scaling, as in the snake example
 const RESOLUTION: (u32, u32) = (2250, 900);
 const NO_RECTS: usize = 150;
+const DESIRED_FPS: u32 = 10;
+const MAX_VALUE: f32 = 750.0;
 
 pub trait Sorter {
     /// Common step interface for sorting algorithms
-    fn step(&mut self);
+    fn step(&mut self, ctx: &Context);
     fn get_arr(&self) -> &Vec<SortElement>;
+    fn is_sorted(&self) -> bool;
 }
 
 struct BubbleSort {
-    // arr: [SortElement; NO_RECTS],
     arr: Vec<SortElement>,
     sorted: bool,
     outer_index: usize,
-    inner_index: usize,
 }
 
 impl BubbleSort {
@@ -37,21 +38,39 @@ impl BubbleSort {
             sorted: false,
             arr: sort_elems,
             outer_index: NO_RECTS,
-            inner_index: 0,
         }
     }
 }
 
 impl Sorter for BubbleSort {
-    fn step(&mut self) {
+    fn step(&mut self, ctx: &Context) {
         // currently if there is not step left between inner and outer index, the step will be
         // empty
-        for i in self.inner_index..self.outer_index - 1 {
+        self.sorted = true;
+        for i in 0..(self.outer_index - 1) {
             if self.arr[i].get_height() > self.arr[i + 1].get_height() {
-                self.arr.swap(i, i + 1);
-                self.sorted = false;
-                self.inner_index = i;
-                return;
+                self.sorted = false; // if we meet another value, we obviously are unsorted
+                let one = self.arr[i].rect;
+                let two = self.arr[i + 1].rect;
+                self.arr[i].rect.h = two.h;
+                self.arr[i + 1].rect.h = one.h;
+
+                self.arr[i].mesh = graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new(one.x, one.y, one.w, two.h),
+                    // self.arr[i + 1].state.get_color(),
+                    Color::GREEN,
+                )
+                .unwrap();
+
+                self.arr[i + 1].mesh = graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new(two.x, two.y, two.w, one.h),
+                    self.arr[i].state.get_color(),
+                )
+                .unwrap();
             }
         }
         self.outer_index -= 1;
@@ -60,6 +79,10 @@ impl Sorter for BubbleSort {
 
     fn get_arr(&self) -> &Vec<SortElement> {
         &self.arr
+    }
+
+    fn is_sorted(&self) -> bool {
+        self.sorted
     }
 }
 
@@ -103,7 +126,7 @@ impl SortElement {
         let mut l_rectangle = graphics::Rect::new(0., 100., 10., 0.);
 
         // we need to reverse the height, since origin is at top left
-        l_rectangle.h = -rng.gen_range(0.0..750.0);
+        l_rectangle.h = -rng.gen_range(0.0..MAX_VALUE);
         l_rectangle.x += X_OFFSET + i as f32 * l_rectangle.w * SCALE;
 
         let l_object = graphics::Mesh::new_rectangle(
@@ -121,8 +144,20 @@ impl SortElement {
     }
 
     fn get_height(&self) -> f32 {
-        self.rect.h
+        -self.rect.h
     }
+
+    // fn swap_with(&mut self, other: &mut SortElement, ctx: &Context) {
+    //     let old_mesh = other.mesh.clone();
+    //     other.mesh = graphics::Mesh::new_rectangle(
+    //         ctx,
+    //         graphics::DrawMode::fill(),
+    //         self.rect,
+    //         self.state.get_color(),
+    //     )
+    //     .unwrap();
+    //     self.mesh = old_mesh;
+    // }
 }
 
 /// Keeps track of current variables and states
@@ -135,8 +170,6 @@ struct GameState<T: Sorter> {
 impl<T: Sorter> GameState<T> {
     fn new(sorter: T, ctx: &mut Context) -> GameResult<GameState<T>> {
         // TODO: (aver) implement scaling for window and ajdust for rendering
-
-        // let l_sorter = BubbleSort::new(ctx);
         Ok(GameState {
             frames: 0,
             sorter: sorter,
@@ -147,7 +180,14 @@ impl<T: Sorter> GameState<T> {
 impl<T: Sorter> event::EventHandler<ggez::GameError> for GameState<T> {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         // TODO: implement update logic
-        // self.sorter.step();
+
+        while _ctx.time.check_update_time(DESIRED_FPS) {
+            if !self.sorter.is_sorted() {
+                self.sorter.step(_ctx);
+            } else {
+                // println!("it's sorted!");
+            }
+        }
         Ok(())
     }
 
@@ -155,9 +195,9 @@ impl<T: Sorter> event::EventHandler<ggez::GameError> for GameState<T> {
         // NOTE: Drawing starts from top left!
 
         let mut canvas = graphics::Canvas::from_frame(ctx, graphics::Color::BLACK);
-
         let mut i = 0;
         let arr = self.sorter.get_arr();
+
         for obj in arr {
             canvas.draw(&obj.mesh, Vec2::new(arr[i].rect.x, RESOLUTION.1 as f32));
             i += 1;
